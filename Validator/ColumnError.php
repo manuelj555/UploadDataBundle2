37204bd1039f1089d1599ccb6e769ad7f7ed8bba
manuel_j555 <programador.manuel@gmail.com>
2014-10-16 09:13:00 -0430

    nueva clase para para agregar errores de validacion

---
+++ Validator/ColumnError.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Manuelj555\Bundle\UploadDataBundle\Validator;
+
+use Symfony\Component\Validator\ConstraintViolation;
+
+/**
+ * @autor Manuel Aguirre <programador.manuel@gmail.com>
+ */
+class ColumnError extends ConstraintViolation
+{
+
+    public function __construct($message, $propertyPath = null, array $messageParameters = array())
+    {
+        parent::__construct($message, $message, $messageParameters, null, $propertyPath, null, null, null);
+    }
+}
