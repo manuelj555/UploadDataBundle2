146f8b4cc1f88b3f5cee0e191f0ec21710cb5f7a
manuelj555 <programador.manuel@gmail.com>
2016-09-16 10:07:54 -0400

    Creado método ColumnsMapper::getDefaultMapping
    Este método permite ontener el mapping por default
    sin la intervención del usuario.
    
    Es util cuando queremos que la carga de archivos sea automática.

--- Mapper/ColumnsMapper.php
+++ Mapper/ColumnsMapper.php
@@ -6,6 +6,7 @@
 
 namespace Manuel\Bundle\UploadDataBundle\Mapper;
 
+use Manuel\Bundle\UploadDataBundle\Mapper\Exception\DefaultMappingException;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -70,6 +71,21 @@ class ColumnsMapper
         return isset($this->labels[$name]) ? $this->labels[$name] : null;
     }
 
+    /**
+     * Retorna un arreglo con las columnas del archivo de datos que se pudieron mapear
+     * de forma automatica con las columnas definidas en el archivo
+     * de configuración.
+     *
+     * Las clases son las columnas definidas en el archivo de configuración
+     * y los values, los nombres  en el archivo de datos.
+     *
+     * <code>
+     *  return ['first_name' => 'First Name'];
+     * </code>
+     *
+     * @param array $fileHeaders
+     * @return array
+     */
     public function match($fileHeaders = array())
     {
         $this->matches = array();
@@ -130,13 +146,91 @@ class ColumnsMapper
         return $this->isMatched($name) ? $this->matches[$name] : null;
     }
 
+    /**
+     * Retorna una matriz con dos posiciones:
+     * 
+     * El primer arreglo es una relacion entre nombres de columna en el archivo y
+     * nombre de columna como se ha definido en el archivo de configuración.
+     * 
+     * El segundo es un arreglo que indica la relacion entre los nombres de columna 
+     * en el archivo y el valor del header leido del archivo.
+     * 
+     * Ejemplo:
+     * 
+     * <code>
+     *     [
+     *         [
+     *             'A' => 'first_name',
+     *             'B' => 'last_name',
+     *             'C' => 'email',
+     *         ],
+     *         [
+     *             'A' => 'First Name',
+     *             'B' => 'Last Name',
+     *             'C' => 'Correo Electrónico',
+     *         ],
+     *     ]
+     * </code>
+     * 
+     * @return array
+     */
     public function mapForm(array $data, array $fileHeaders)
     {
-//        $data = array_flip($data);
-//        ksort($data);
-
         return array(array_flip($data), $fileHeaders);
+    }
 
-//        return array_combine($data, $fileHeaders);
+    /**
+     * Retorna el mapping por defecto en base a las columnas definidas y a los headers del archivo.
+     *
+     * Esta funcion es muy útil cuando se tiene la certesa de que el archivo subido comple con las
+     * columnas esperadas tanto en cantidad como en nombre.
+     *
+     * Es usada por ejemplo cuando la lectura del excel no la hace el usuario sino que va a ser automática.
+     *
+     * @param array $fileHeaders
+     * @return array Ejemplo:
+     *
+     * <code>
+     *     [
+     *         [
+     *             'A' => 'first_name',
+     *             'B' => 'last_name',
+     *             'C' => 'email',
+     *         ],
+     *         [
+     *             'A' => 'First Name',
+     *             'B' => 'Last Name',
+     *             'C' => 'Correo Electrónico',
+     *         ],
+     *     ]
+     * </code>
+     *
+     * @throws DefaultMappingException si no se pudo hacer un match completo de las columnas se lanza
+     * esta exception.
+     */
+    public function getDefaultMapping(array $fileHeaders)
+    {
+        $matches = $this->match($fileHeaders);
+
+        if(count($matches) !== count($this->getColumns())){
+            throw new DefaultMappingException(sprintf('Default Mapping requires equals elements count for file headers (%d) and matches result (%d)', count($fileHeaders), count($this->getColumns())));
+        }
+
+        $matchedInFile = array_intersect($matches, $fileHeaders);
+        $filteredHeaders = array_intersect($fileHeaders, $matchedInFile);
+
+        // Verificamos que los dos arreglos esten ordenados de igual forma.
+        asort($matchedInFile);
+        asort($filteredHeaders);
+
+        $values = array_keys($matchedInFile);
+        $keys = array_keys($filteredHeaders);
+        // combinamos los arreglos, del archivo sacamos las keys y del match los values.
+        $expectedColumns = array_combine($keys, $values);
+
+        // Ordenamos por las claves para que concuerde con los headers en el archivo.
+        ksort($expectedColumns);
+
+        return array($expectedColumns, $fileHeaders);
     }
 }
---
+++ Mapper/Exception/DefaultMappingException.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of the Manuel Aguirre Project.
+ *
+ * (c) Manuel Aguirre <programador.manuel@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Manuel\Bundle\UploadDataBundle\Mapper\Exception;
+
+/**
+ * @author Manuel Aguirre <programador.manuel@gmail.com>
+ */
+class DefaultMappingException extends \LogicException
+{
+
+}
